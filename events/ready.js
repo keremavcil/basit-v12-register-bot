f447a65771d9979309b946457ca16f650108c0c0
atlas <77780284+atlasexe@users.noreply.github.com>
2021-05-21 22:14:03 +0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,5 @@
 module.exports = client => {
   client.user.setStatus("idle");
   console.log(`${client.user.username} adı ile giriş yapıldı!`) 
-client.user.setActivity(`developed by ' atlas star. ✬#7730`, { type: "LISTENING"});
+client.user.setActivity(`developed by ' atlas. ✵ ✬ #7730`, { type: "LISTENING"});
 };
